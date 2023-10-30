390663762810e3ad44791e10b935a118c537262c
Ernest Guevarra <ernest@guevarra.io>
2023-10-30 05:08:25 +0000

    initial commit

---
+++ R/oxfordtheme-package.R
@@ -0,0 +1,15 @@
+#
+#'
+#' University of Oxford Theme and Theme Components for 'ggplot2'
+#'
+#' Theme and theme components for 'ggplot2' based on University of Oxford's
+#' visual identity guidelines <https://www.ox.ac.uk/sites/files/oxford/media_wysiwyg/Oxford%20Blue%20LR.pdf>.
+#'
+#'
+#' @md
+#' @name oxfordtheme
+#' @docType package
+#' @author Ernest Guevarra (ernest@@guevarra.io)
+#' @keywords internal
+#
+"_PACKAGE"
